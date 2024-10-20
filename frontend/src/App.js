34df34810ae4360eae4857e0ded8b8e91e9d5460
Engineer-kim <ddang0103@naver.com>
2024-10-20 18:11:00 +0900

    Feature: 누락 파일 커밋

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { RouterProivder, createBrowserRouter, parsePath } from 'react-router-dom
 import HomePage from './pages/Home'
 import EventPages, { loader as eventLoader } from './pages/Events';
 import NewEventPages from './pages/NewEvent';
-import EventDetailPages , { loader as eventDetailLoader } from './pages/EventDetail';
+import EventDetailPages, { loader as eventDetailLoader } from './pages/EventDetail';
 import EventEditPages from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventRootLayout from './pages/EventRoot';
@@ -12,17 +12,27 @@ import ErrorPage from './pages/Error';
 const router = createBrowserRouter([
   {
     path: '/', element: <RootLayout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: 'event', element: <EventRootLayout />, children: [
           {
-            index: true, element: <EventPages />, loader: eventDetailLoader
+            index: true, element: <EventPages />, loader: eventLoader
           },
-          { path: ':eventId', element: <EventDetailPages />, loader },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPages />
+              },
+              { path: 'edit', element: <EventEditPages /> }
+            ]
+          },
           { path: 'new', element: <NewEventPages /> },
-          { path: ':eventId/edit', element: <EventEditPages /> }
         ]
       }
     ],
