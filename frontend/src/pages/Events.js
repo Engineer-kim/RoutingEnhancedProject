5c32993a8448027e1b5c96c4293e58c990ecba02
Engineer-kim <ddang0103@naver.com>
2024-10-21 20:59:30 +0900

    Feature: 인증 컴포넌트 및 벡엔드의 인증 로직 추가

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,31 +1,44 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
+
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    const data = useLoaderData();
+  const { events } = useLoaderData();
 
-    // if(data.isError){
-    //     return <p>{data}</p>
-    // }
-
-    const events = data.events
-
-    return (
-        <>
-            <EventsList events={events} />
-        </>
-    );
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
-    const response = await fetch('http://localhost:8080/events');
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
 
-    if (!response.ok) {
-        // throw new Response(JSON.stringify({message: 'Could Not Fetch Event'}, {status: 500}))
-        return json({message: 'Could Not Fetch Event'} , {status: 500})
-    } else {
-      return response
-    }
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: 'Could not fetch events.' },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,20 +1,19 @@
-import { Outlet, useNavigation } from "react-router-dom"
-import MainNavigation from "../components/MainNavigation"
+import { Outlet, useNavigation } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
-    // const navigation = useNavigation();
+  // const navigation = useNavigation();
 
-
-    return (
-        <>
-            <MainNavigation />
-            <main>
-                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-                <Outlet />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
-
-export default RootLayout
+export default RootLayout;
