b0aa446505fe6c1c1f70dc8e9e5977bd196d3873
Engineer-kim <ddang0103@naver.com>
2024-10-21 22:45:03 +0900

    Feature: 로그아웃 기능 및 토큰에 대한 유효시간(1시간으로 설정) , 토큰 여부 판단으로 특정 메뉴(URI) 비로그인시 비노출 처리

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,20 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet ,userLodaerData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+  const token = userLodaerData();
+  const submit = useSubmit()
+  useEffect(()=>{
+    if(!token){
+      return 
+    }
+
+    setTimeout(() => {
+      submit(null, {action: '/logout' , method: 'post'})
+    }, 1 * 60 * 60 * 1000)  //==> 1hour To MilliSecond
+  },[token ,submit])
 
   return (
     <>
